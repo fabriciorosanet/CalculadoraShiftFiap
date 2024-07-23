bff965f135e92a733fa19ef310de0d78892ba8bd
Fabricio Rosa <fabriciorosafiap@gmail.com>
2024-07-23 18:42:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double n1, n2, res;
+Console.WriteLine("Digite o primeiro número: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digitte o segundo número: ");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+res = n1+n2;
+Console.WriteLine("Soma: {0}", res.ToString());
+
+res = n1 - n2;
+Console.WriteLine("Subtração: {0}", res);
+
+res = n1 * n2;
+Console.WriteLine("Multiplicação: {0}", res);
+
+if (n2 != 0)
+{
+    res = n1 / n2;
+    Console.WriteLine("Divisão: {0}", res);
+}
+else
+{
+    Console.WriteLine("Divisão: Não é possível dividir por zero.");
+}
